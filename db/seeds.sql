7126db82f478660a03f6e2b6162af84150e162e3
StarLord <chrisannfalk@gmail.com>
2018-08-18 16:19:42 -0500

    updates

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name) VALUES ('Southern Burger');
+INSERT INTO burgers(burger_name) VALUES ('Bison Burger');
+INSERT INTO burgers(burger_name) VALUES ('Black Bean Burger');
+INSERT INTO burgers(burger_name) VALUES ('Peppermit Cheeseburger');
+INSERT INTO burgers(burger_name) VALUES ('7 Day Old Hamburger');
